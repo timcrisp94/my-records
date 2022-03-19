15f4582e6bcd3e9259cad4b5a95951d666d94aa3
Tim Crisp <timothycrisp@gmail.com>
2022-03-19 12:15:52 -0500

    add css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { Route, Routes, NavLink } from 'react-router-dom'
+import './App.css'
 
 function App() {
+  const [records, setRecords] = useState([])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Records
+        <nav>
+          <NavLink to='/'>Records</NavLink>
+          <NavLink className='m-left' to='/add'>Add Record</NavLink>
+        </nav>
       </header>
+      <main>
+        
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
